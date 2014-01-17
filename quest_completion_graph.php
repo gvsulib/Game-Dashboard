b9c53fd1b71ac51a9b72417c5e96ea82a238c1a6
Jon Earley <Jonathan.a.earley@gmail.com>
2014-01-17 13:16:11 -0500

    Fixed time display errors for the quest and new player graphs

--- quest_completion_graph.php
+++ quest_completion_graph.php
@@ -18,29 +18,46 @@
       $minDate = $row['MIN(weekNumber)'];
    }
 
-   $endDate = date("W");
-   $infoDate = $endDate - 12; // 13 dates of data
+   $endDateYear = date("Y");
+   $endDateWeek = date("W");
 
-   while ($infoDate <= $endDate) {
+   $infoDateYear = date("Y", strtotime("-13 week"));
+   $infoDateWeek = date("W", strtotime("-13 week"));
+
+
+   $build_graph = true;
+   
+   while ($build_graph == true) {
 
       $result = $db->query("SELECT WEEK(quest_progresses.updated_at) as weekNumber, YEAR(quest_progresses.updated_at) as yearNumber, COUNT(quest_progresses.completed) as count
             FROM quest_progresses
-            WHERE quest_progresses.completed = 1 AND WEEK(quest_progresses.updated_at) = '$infoDate' ORDER BY yearNumber DESC");
+            WHERE quest_progresses.completed = 1 AND WEEK(quest_progresses.updated_at) = '$infoDateWeek' ORDER BY yearNumber DESC");
 
       while($row = $result->fetch_assoc()) {
+
+        $infoDateWeek = sprintf("%02s", $infoDateWeek);
 
          echo '
          <li>
            <a href="#">
             <span class="count" style="height: ' . $row['count'] / $maxCompleted * 100 . '%">(' . $row['count'] . ')</span>
             <span class="label">' . ($row['count'] > 0 ? $row['count'] : '') . '</span>
-            <span class="label-data">' . date("n/j", strtotime('2013W' . $infoDate)) . '</span>
+            <span class="label-data">' . date("M d", strtotime($infoDateYear . "W" . $infoDateWeek)) . '</span>
            </a>
          </li>
          ';
       }
 
-      $infoDate++;
+        if ($infoDateWeek >= 52) {
+            $infoDateWeek = 1;
+            $infoDateYear++;
+         } else {
+            $infoDateWeek++;
+         }
+
+         if ($infoDateYear >= $endDateYear && $infoDateWeek >= $endDateWeek) {
+            $build_graph = false;
+         }
    } 
 
    ?>

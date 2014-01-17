b70ee5197b7caf70213e021b46223450becb972d
Jon Earley <Jonathan.a.earley@gmail.com>
2014-01-17 13:06:37 -0500

    Attempting to fix time display errors

--- new_players_graph.php
+++ new_players_graph.php
@@ -16,34 +16,57 @@
       $maxCount = $row['MAX(count)'];
    }
 
-   $endDate = date("W");
-   $infoDate = $endDate - 12; // 13 dates of data
+   $endDateYear = date("Y");
+   $endDateWeek = date("W");
 
+   $infoDateYear = date("Y", strtotime("-13 week"));
+   $infoDateWeek = date("W", strtotime("-13 week"));
 
-   while ($infoDate <= $endDate) {
+   /*
+   echo '<br>$endDateYear: ' . $endDateYear;
+   echo '<br>$endDateWeek: ' . $endDateWeek;
+   echo '<br>$infoDateYear: ' . $infoDateYear;
+   echo '<br>$infoDateWeek: ' . $infoDateWeek;
+   echo '<br>';
+   */
+
+   $build_graph = true;
+   
+   while ($build_graph == true) {
 
       $result = $db->query("SELECT WEEK(users.created_at) as weekNumber, YEAR(users.created_at) as yearNumber, COUNT(users.id) as count
             FROM users
-            WHERE WEEK(users.created_at) = '$infoDate'
+            WHERE WEEK(users.created_at) = '$infoDateWeek'
             ORDER BY yearNumber DESC");
 
       while($row = $result->fetch_assoc()) {
 
-
+         $infoDateWeek = sprintf("%02s", $infoDateWeek);
 
          echo '
          <li>
            <a href="#">
             <span class="count" style="height: ' . $row['count'] / $maxCount * 100 . '%">(' . $row['count'] . ')</span>
             <span class="label">' . ($row['count'] > 0 ? $row['count'] : '') . '</span>
-            <span class="label-data">' . date("n/j", strtotime('2013W' . $infoDate)) . '</span>
+            <span class="label-data">' . date("M d", strtotime($infoDateYear . "W" . $infoDateWeek)) . '</span>
            </a>
          </li>
          ';
-      }
 
-      $infoDate++;
+         if ($infoDateWeek >= 52) {
+            $infoDateWeek = 1;
+            $infoDateYear++;
+         } else {
+            $infoDateWeek++;
+         }
+
+         if ($infoDateYear >= $endDateYear && $infoDateWeek >= $endDateWeek) {
+            $build_graph = false;
+         }
+
+      }
    } 
+   
 
    ?>
 
